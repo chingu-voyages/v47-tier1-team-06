96af94905a1dacbf365581181d4b3996bbb402c2
WildStyle <9sanchez@protonmail.com>
2024-01-19 00:49:13 -0800

    updated index html code, updated main js code for task buttons.

--- src/js/main.js
+++ src/js/main.js
@@ -1,235 +1,200 @@
 const tasks = [
-	{
-		"categoryName": "ROUTINE ACTIVITIES",
-		"activityTypes": [
-			{
-				"activityName": "Projects",
-				"Tasks": [
-					{
-						"taskName": "Update Recipes Project Backlog",
-						"taskDescription": "",
-						"days": [
-							"monday"
-						]
-					},
-					{
-						"taskName": "Update The dailyTasks sheet with the backlog tasks",
-						"taskDescription": "add the filtering feature to Done",
-						"days": [
-							"monday"
-						]
-					}
-				]
-			},
-			{
-				"activityName": "Blog Posts",
-				"Tasks": [
-					{
-						"taskName": "Publish The recent Blog Post Draft to hashnode",
-						"taskDescription": "",
-						"days": [
-							"friday"
-						]
-					},
-					{
-						"taskName": "Write a New headline in a Blog Post Draft",
-						"taskDescription": "",
-						"days": [
-							"saturday",
-							"sunday",
-							"monday",
-							"tuesday",
-							"wednesday",
-							"thursday",
-							"friday"
-						]
-					}
-				]
-			}
-		]
-	},
-	{
-		"categoryName": "STUDYING",
-		"activityTypes": [
-			{
-				"activityName": "Node Js Course",
-				"Tasks": [
-					{
-						"taskName": "Plan The Node Js Course Progress By Month",
-						"taskDescription": "Set Up A Plan For The Next Month Of Node Js Learning",
-						"days": [
-							"1"
-						]
-					},
-					{
-						"taskName": "Study The First Node Js Lecture",
-						"taskDescription": "",
-						"days": [
-							"2"
-						]
-					}
-				]
-			},
-			{
-				"activityName": "MongoDB",
-				"Tasks": [
-					{
-						"taskName": "Plan The MongoDB Course Progress By Month",
-						"taskDescription": "Set Up A Plan For The Next Month Of Node Js Learning",
-						"days": [
-							"30"
-						]
-					},
-					{
-						"taskName": "Study The First MongoDB Lecture",
-						"taskDescription": "",
-						"days": [
-							"15"
-						]
-					}
-				]
-			}
-		]
-	},
-	{
-		"categoryName": "DAILY TASKS PROJECT",
-		"activityTypes": [
-			{
-				"activityName": "Backlog",
-				"Tasks": [
-					{
-						"taskName": "Add The New Features list",
-						"taskDescription": "",
-						"days": [
-							"monday"
-						]
-					},
-					{
-						"taskName": "Add The New PRs To InReview",
-						"taskDescription": "",
-						"days": [
-							"7"
-						]
-					}
-				]
-			},
-			{
-				"activityName": "Coding",
-				"Tasks": [
-					{
-						"taskName": "Work On The Sidebar",
-						"taskDescription": "Add The Sections Links",
-						"days": [
-							"thursday"
-						]
-					},
-					{
-						"taskName": "Refactor The Filtering Feature Code",
-						"taskDescription": "",
-						"days": [
-							"friday"
-						]
-					}
-				]
-			}
-		]
-	},
-	{
-		"categoryName": "CHINGU",
-		"activityTypes": [
-			{
-				"activityName": "Voyage",
-				"Tasks": [
-					{
-						"taskName": "Conduct The Project Planning Meeting",
-						"taskDescription": "Conduct The Project Planning Meeting To Discuss Our Ideas",
-						"days": [
-							"monday"
-						]
-					},
-					{
-						"taskName": "Create The UI/UX Design For The DailyTasks Project",
-						"taskDescription": "Create The UI/UX Design For The DailyTasks Project Based On The Team Discussion",
-						"days": [
-							"monday",
-							"tuesday",
-							"wednesday"
-						]
-					}
-				]
-			},
-			{
-				"activityName": "Pair Programming",
-				"Tasks": [
-					{
-						"taskName": "Create When2Meet Link",
-						"taskDescription": "Create When2Meet Link To Introduce Yourselves",
-						"days": [
-							"thursday"
-						]
-					},
-					{
-						"taskName": "Attend The Introduction Meeting With Someone",
-						"taskDescription": "",
-						"days": [
-							"thursday"
-						]
-					}
-				]
-			}
-		]
-	}
+  {
+    categoryName: "ROUTINE ACTIVITIES",
+    activityTypes: [
+      {
+        activityName: "Projects",
+        Tasks: [
+          {
+            taskName: "Update Recipes Project Backlog",
+            taskDescription: "",
+            days: ["monday"],
+          },
+          {
+            taskName: "Update The dailyTasks sheet with the backlog tasks",
+            taskDescription: "add the filtering feature to Done",
+            days: ["monday"],
+          },
+        ],
+      },
+      {
+        activityName: "Blog Posts",
+        Tasks: [
+          {
+            taskName: "Publish The recent Blog Post Draft to hashnode",
+            taskDescription: "",
+            days: ["friday"],
+          },
+          {
+            taskName: "Write a New headline in a Blog Post Draft",
+            taskDescription: "",
+            days: [
+              "saturday",
+              "sunday",
+              "monday",
+              "tuesday",
+              "wednesday",
+              "thursday",
+              "friday",
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    categoryName: "STUDYING",
+    activityTypes: [
+      {
+        activityName: "Node Js Course",
+        Tasks: [
+          {
+            taskName: "Plan The Node Js Course Progress By Month",
+            taskDescription:
+              "Set Up A Plan For The Next Month Of Node Js Learning",
+            days: ["1"],
+          },
+          {
+            taskName: "Study The First Node Js Lecture",
+            taskDescription: "",
+            days: ["2"],
+          },
+        ],
+      },
+      {
+        activityName: "MongoDB",
+        Tasks: [
+          {
+            taskName: "Plan The MongoDB Course Progress By Month",
+            taskDescription:
+              "Set Up A Plan For The Next Month Of Node Js Learning",
+            days: ["30"],
+          },
+          {
+            taskName: "Study The First MongoDB Lecture",
+            taskDescription: "",
+            days: ["15"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    categoryName: "DAILY TASKS PROJECT",
+    activityTypes: [
+      {
+        activityName: "Backlog",
+        Tasks: [
+          {
+            taskName: "Add The New Features list",
+            taskDescription: "",
+            days: ["monday"],
+          },
+          {
+            taskName: "Add The New PRs To InReview",
+            taskDescription: "",
+            days: ["7"],
+          },
+        ],
+      },
+      {
+        activityName: "Coding",
+        Tasks: [
+          {
+            taskName: "Work On The Sidebar",
+            taskDescription: "Add The Sections Links",
+            days: ["thursday"],
+          },
+          {
+            taskName: "Refactor The Filtering Feature Code",
+            taskDescription: "",
+            days: ["friday"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    categoryName: "CHINGU",
+    activityTypes: [
+      {
+        activityName: "Voyage",
+        Tasks: [
+          {
+            taskName: "Conduct The Project Planning Meeting",
+            taskDescription:
+              "Conduct The Project Planning Meeting To Discuss Our Ideas",
+            days: ["monday"],
+          },
+          {
+            taskName: "Create The UI/UX Design For The DailyTasks Project",
+            taskDescription:
+              "Create The UI/UX Design For The DailyTasks Project Based On The Team Discussion",
+            days: ["monday", "tuesday", "wednesday"],
+          },
+        ],
+      },
+      {
+        activityName: "Pair Programming",
+        Tasks: [
+          {
+            taskName: "Create When2Meet Link",
+            taskDescription: "Create When2Meet Link To Introduce Yourselves",
+            days: ["thursday"],
+          },
+          {
+            taskName: "Attend The Introduction Meeting With Someone",
+            taskDescription: "",
+            days: ["thursday"],
+          },
+        ],
+      },
+    ],
+  },
 ];
 
-const test = (t)=> {
-	try{
-		console.log(t);
-	}catch(error){
-		console.log(error);
-	};
+const test = (t) => {
+  try {
+    console.log(t);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-function hideAll(){
-	const forms = document.getElementsByClassName("forms");
-	
-	test(forms.style);
-};
+function hideAll() {
+  const forms = document.getElementsByClassName("forms");
+
+  test(forms.style);
+}
 function showAdd() {
-	console.log('inside showAdd');
-	// const addForm = document.getElementsByClassName("addForm");
-	// const show = ()=>addForm[0].style.visibility = "visible";
-	// show(addForm);
-	hideAll();
+  console.log("inside showAdd");
+  // const addForm = document.getElementsByClassName("addForm");
+  // const show = ()=>addForm[0].style.visibility = "visible";
+  // show(addForm);
+  hideAll();
 }
 
-const showEdit = ()=> {
-	console.log('edit button clicked')
-
+const showEdit = () => {
+  console.log("edit button clicked");
 };
 
-const showDelete = ()=> {
-	console.log('delete button clicked')
-
+const showDelete = () => {
+  console.log("delete button clicked");
 };
 
-function addTask(){
-	console.log('inside task note');
-	addElement();
+function addTask() {
+  console.log("inside task note");
+  addElement();
+}
 
-};
-
-function addElement(){
-	console.log('inside addElement');
-	const tasks = document.querySelector(".tasks");
-	const task = document.createElement("div");
-	task.className="task";
-	tasks.appendChild(task);
-    test(task);
-};
-
-
-
-
+function addElement() {
+  console.log("inside addElement");
+  const tasks = document.querySelector(".tasks");
+  const task = document.createElement("div");
+  task.className = "task";
+  tasks.appendChild(task);
+  test(task);
+}
 
 /*
 try{

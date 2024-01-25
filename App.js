8c9b4252ab3418976dd99e2effa90dbd8e8570da
WildStyle <9sanchez@protonmail.com>
2024-01-24 18:30:25 -0800

    pull update frm main to maya-work.

--- App.js
+++ App.js
@@ -1,1 +1,25 @@
-/*Mainjs Files here*/
+function showTaskDetails(taskName, taskDate) {
+    const detailsContainer = document.getElementById('taskDetails');
+    detailsContainer.innerHTML = `<p>Task: ${taskName}</p><p>Date: ${taskDate}</p>`;
+}
+function addNewTask() {
+    const task = prompt("Enter a new task");
+    if (task) {
+        addTaskToList(task);
+    }
+}
+
+function addTaskToList(task) {
+    const taskList = document.getElementsByClassName('list')[0]; 
+    const newTask = document.createElement("li");
+    newTask.innerHTML = "&#10003; " + task;
+    taskList.appendChild(newTask);
+}
+
+function addTaskForDate(date) {
+    const task = prompt("Enter task for " + date);
+    if (task) {
+        addTaskToList(task + " - " + date);
+    }
+}
+
